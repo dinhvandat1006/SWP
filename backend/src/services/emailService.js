a8761c79260c8780f95328f663d45f9439ee8e0f
de180551chauvuonghoang-svg <de180551chauvuonghoang@gmail.com>
2026-01-10 22:44:37 +0700

    fix emailService

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -12,7 +12,9 @@ const createTransporter = () => {
   }
 
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
